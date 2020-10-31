03e3f9aee0fcf5420f203a559ca873515a12f6b3
Yael Keemink <Yael.Keemink@infosupport.com>
2020-10-31 14:24:16 +0100

    Add angular app

---
+++ VeganStartPaginaFE/vegan-startpagina/src/app/WordVegan/WordVegan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-WordVegan',
+  templateUrl: './WordVegan.component.html',
+  styleUrls: ['./WordVegan.component.scss']
+})
+export class WordVeganComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
